df35216b7fa22787a54798f5912347503412f020
MaryKwawu <may419316@gmail.com>
2023-06-05 12:12:31 +0000

    developed a band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+greetings = "Welcome to my band name generator app \n"
+print(greetings)
+
+city_name = input("which city did you grew up in? \n")
+# print(city_name)
+
+pet_name = input("What is the name of your pet? \n")
+
+band_name_generator = "Your band name could be: \n " + "" + str(city_name) + " " + str(pet_name)
+print(band_name_generator)
