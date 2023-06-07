a447ddaf765ba810779d024236fedd87f7557a02
MaryKwawu <may419316@gmail.com>
2023-06-07 04:37:28 +0000

    Day2 of 100 Days of code

---
+++ tip_calculator/tip_calculator.py
@@ -0,0 +1,22 @@
+# If the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+
+greetings = "Welcome to my tip calculator buddy :) \n"
+print(greetings)
+
+bill = float(input("What was my total bill? \n"))
+
+bill_tip = int(input("How much of the tip will you like to give? 10 , 12 or 15? \n"))
+
+bill_split = int(input("How many people will split the bill? \n"))
+
+bill_payment = round(bill_tip / 100 * bill + bill )
+# bill_payment = "{:.2f}".format(bill_tip / 100 * bill + bill)
+print(f"Each person should pay:${bill_payment}")
+
+
